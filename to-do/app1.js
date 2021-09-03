b9712763f7dc9d657f0d69125fb5c428e0a47a69
E2-Naganandhini <naganandhini@e2infosystems.com>
2021-09-03 10:07:57 +0530

    After changes in nav bar add and delete functionality

--- to-do/app1.js
+++ to-do/app1.js
@@ -1,325 +1,249 @@
-//add Container
-var container2 = document.querySelector(".container2");
-var container3 = document.querySelector(".container3");
-var notes = document.querySelector(".notes");
+var container = document.querySelector(".container");
 var notification = document.getElementById("del");
 const selctorNav = document.querySelectorAll(".nav-btn li");
-console.log(selctorNav);
 var addButton = document.querySelector(".add-btn");
-var container = document.querySelector(".container");
 //get elemnet from local Storage
 var todosList = JSON.parse(localStorage.getItem("todos")) || [];
-console.log(todosList);
-if (todosList.length > 0) {
-  console.log("list");
-  displayTask();
-}
-
+displayTask();
 //add element
+var addInputBtn = document.querySelector(".inpUpdate");
+var inputClose = document.querySelector(".inpClose");
+var addErrorMsg = document.querySelector(".addErr");
+var inputModel = document.querySelector(".inputModel");
+var enteredInput = document.querySelector(".editInp");
+inputClose.onclick = () => {
+	inputModel.style.display = "none";
+};
+var cancelInpBtn = document.querySelector(".inpCancel");
+cancelInpBtn.onclick = () => {
+	inputModel.style.display = "none";
+};
 addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  var addInputBtn = document.querySelector(".inpUpdate");
-  var inputClose = document.querySelector(".inpClose");
-  var enteredInput = document.querySelector(".editInp");
-
-  var cancelInpBtn = document.querySelector(".inpCancel");
-  var addErrorMsg = document.querySelector(".addErr");
-  var inputModel = document.querySelector(".inputModel");
-  inputModel.style.display = "block";
-  enteredInput.focus();
-  addInputBtn.addEventListener("click", () => {
-    if (enteredInput.value === "") {
-      addErrorMsg.innerHTML = "Add Your Task";
-      setTimeout(function () {
-        addErrorMsg.innerHTML = "";
-      }, 1000);
-    } else {
-      var input = enteredInput.value;
-      addTaskToLocalStore(input);
-      inputModel.style.display = "none";
-      enteredInput.value = "";
-      todosList = JSON.parse(localStorage.getItem("todos")) || [];
-      notification.innerHTML = "Added successfully";
-      setTimeout(function () {
-        notification.innerHTML = "";
-      }, 2000);
-    }
-  });
-  inputClose.addEventListener("click", () => {
-    inputModel.style.display = "none";
-  });
-  cancelInpBtn.addEventListener("click", () => {
-    inputModel.style.display = "none";
-  });
+	e.preventDefault();
+	inputModel.style.display = "block";
+	enteredInput.focus();
+	addInputBtn.addEventListener("click", () => {
+		if (enteredInput.value === "") {
+			addErrorMsg.innerHTML = "Add Your Task";
+			setTimeout(function () {
+				addErrorMsg.innerHTML = "";
+			}, 1000);
+		} else {
+			var input = enteredInput.value;
+			addTaskToLocalStore(input);
+			inputModel.style.display = "none";
+			enteredInput.value = "";
+			todosList = JSON.parse(localStorage.getItem("todos")) || [];
+			delMsg((msg = "Added"));
+		}
+	});
 });
-
 //add task to local storage
 function addTaskToLocalStore(input) {
-  todosList = JSON.parse(localStorage.getItem("todos")) || [];
-  todosList.push({
-    text: input,
-    completed: false,
-  });
-  localStorage.setItem("todos", JSON.stringify(todosList));
-  displayTask();
+	todosList.push({
+		text: input,
+		completed: false,
+	});
+	localStorage.setItem("todos", JSON.stringify(todosList));
+	displayTask();
 }
-if (todosList.length === 0) {
-  container.innerHTML = "No Task";
+//create element
+function createElement(element, clsName, Id, innerText) {
+	var el = document.createElement(element);
+	el.classList.add(clsName);
+	el.id = Id;
+	el.innerHTML = innerText;
+	return el;
 }
-// display task
+//display tasks
 function displayTask() {
-  container2.style.display = "none";
-  container3.style.display = "none";
-  container.style.display = "block";
-  container.innerHTML = "";
-  notes.style.display = "block";
+	container.innerHTML = "";
+	if (todosList.length == 0) {
+		container.innerHTML = "No Task";
+	}
+	if (todosList.length === 0) {
+		container.innerHTML = "No Task";
+	}
+	todosList.forEach((todoList) => {
+		var itemBox = document.createElement("div");
+		itemBox.classList.add("item");
+		var todoTxt = todoList.text;
+		var todoElement = createElement("li", "item_input", "placing", todoTxt);
+		console.log(todoElement);
+		todoElement.onclick = (e) => {
+			e.preventDefault();
+			todoElement.classList.toggle("completed");
+			updateTaskToLocalStore();
+		};
+		if (todoList && todoList.completed) {
+			todoElement.classList.add("completed");
+		}
 
-  if (todosList.length === 0) {
-    container.innerHTML = "No Task";
-  }
-  todosList.forEach((todoList) => {
-    var itemBox = document.createElement("div");
-    itemBox.classList.add("item");
-    var todoElement = document.createElement("li");
-    todoElement.innerHTML = todoList.text;
-    todoElement.classList.add("item_input");
-    todoElement.id = "placing";
+		var editBtn = createElement("button", "edit", "placing", "EDIT");
+		editBtn.onclick = () => {
+			editElement(todoElement);
+		};
 
-    todoElement.addEventListener("click", (e) => {
-      e.preventDefault();
-      todoElement.classList.toggle("completed");
-      updateTaskToLocalStore();
-    });
+		var removeBtn = createElement("button", "delete", "placing", "DELETE");
+		removeBtn.onclick = () => {
+			removeElement(itemBox, todoList.text);
+		};
 
-    if (todoList && todoList.completed) {
-      todoElement.classList.add("completed");
-    }
-
-    var editBtn = document.createElement("button");
-    editBtn.classList.add("edit");
-    editBtn.innerHTML = "EDIT";
-    editBtn.id = "placing";
-    editBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      editElement(todoElement);
-    });
-
-    var removeBtn = document.createElement("button");
-    removeBtn.innerHTML = "DELETE";
-    removeBtn.classList.add("delete");
-    removeBtn.id = "placing";
-    removeBtn.addEventListener("click", () => {
-      removeElement(itemBox, todoList.text);
-    });
-
-    itemBox.appendChild(todoElement);
-    itemBox.appendChild(editBtn);
-    itemBox.appendChild(removeBtn);
-    container.appendChild(itemBox);
-  });
+		itemBox.appendChild(todoElement);
+		itemBox.appendChild(editBtn);
+		itemBox.appendChild(removeBtn);
+		container.appendChild(itemBox);
+	});
 }
-
+//edit function
 function editElement(todoEl) {
-  var updateModel = document.querySelector(".edit-model");
-  var userInputBox = document.querySelector(".edit-input");
-  var errorMsg = document.querySelector(".editErr");
-  var updateBtn = document.querySelector(".edit-update");
-  var upCancelBtn = document.querySelector(".edit-cancel");
-  var upCloseBtn = document.querySelector(".edit-Close");
-  var todoText = todoEl.innerHTML;
-  for (var i = 0; i < todosList.length; i++) {
-    console.log(todosList[i].text);
-    if (todosList[i].text === todoText) {
-      index = i;
-      break;
-    }
-  }
-  if (todosList[index].completed === false) {
-    updateModel.style.display = "block";
-    userInputBox.value = todosList[index].text;
-    userInputBox.focus();
-    updateBtn.addEventListener("click", () => {
-      console.log(userInputBox.value);
-      if (userInputBox.value === "") {
-        errorMsg.innerHTML = "Enter your task";
-        setTimeout(function () {
-          errorMsg.innerHTML = "";
-        }, 2000);
-      } else {
-        todosList[index].text = userInputBox.value;
-        window.localStorage.setItem("todos", JSON.stringify(todosList));
-        //todoEl.innerHTML = userInputBox.value;
-        updateModel.style.display = "none";
-        displayTask();
-      }
-    });
-    upCancelBtn.addEventListener("click", () => {
-      updateModel.style.display = "none";
-    });
-    upCloseBtn.addEventListener("click", () => {
-      updateModel.style.display = "none";
-    });
-  } else {
-    var modal2 = document.querySelector(".modal1");
-    var span1 = document.querySelector(".close1");
-    var ok = document.querySelector(".ok");
-    modal2.style.display = "block";
-    span1.addEventListener("click", () => {
-      modal2.style.display = "none";
-    });
-    ok.addEventListener("click", () => {
-      modal2.style.display = "none";
-    });
-  }
+	var updateModel = document.querySelector(".edit-model");
+	var userInputBox = document.querySelector(".edit-input");
+	var errorMsg = document.querySelector(".editErr");
+	var updateBtn = document.querySelector(".edit-update");
+	var upCancelBtn = document.querySelector(".edit-cancel");
+	var upCloseBtn = document.querySelector(".edit-Close");
+	var todoText = todoEl.innerHTML;
+	for (var i = 0; i < todosList.length; i++) {
+		if (todosList[i].text === todoText) {
+			index = i;
+			break;
+		}
+	}
+	if (todosList[index].completed === false) {
+		updateModel.style.display = "block";
+		userInputBox.value = todosList[index].text;
+		userInputBox.focus();
+		updateBtn.addEventListener("click", () => {
+			if (userInputBox.value === "") {
+				errorMsg.innerHTML = "Enter your task";
+				setTimeout(function () {
+					errorMsg.innerHTML = "";
+				}, 2000);
+			} else {
+				todosList[index].text = userInputBox.value;
+				window.localStorage.setItem("todos", JSON.stringify(todosList));
+				updateModel.style.display = "none";
+				displayTask();
+			}
+		});
+		upCancelBtn.addEventListener("click", () => {
+			updateModel.style.display = "none";
+		});
+		upCloseBtn.addEventListener("click", () => {
+			updateModel.style.display = "none";
+		});
+	} else {
+		var modal2 = document.querySelector(".modal1");
+		var span1 = document.querySelector(".close1");
+		var ok = document.querySelector(".ok");
+		modal2.style.display = "block";
+		span1.addEventListener("click", () => {
+			modal2.style.display = "none";
+		});
+		ok.addEventListener("click", () => {
+			modal2.style.display = "none";
+		});
+	}
 }
-
+//remove function
 function removeElement(itemBox, todoText) {
-  var index = -1;
-  for (var i = 0; i < todosList.length; i++) {
-    if (todosList[i].text === todoText) {
-      index = i;
-      break;
-    }
-  }
-  var delModel = document.querySelector(".modal");
-  var delModelContent = document.querySelector(".delP");
-  var delBtn = document.querySelector(".del1");
-  var delCanBtn = document.querySelector(".cancel1");
-  var delClose = document.querySelector(".close");
-  //delete if task completed
-  if (todosList[index].completed === true) {
-    delModelContent.innerHTML = "Are your Sure?";
-    delModel.style.display = "block";
-    delBtn.addEventListener("click", () => {
-      console.log(itemBox);
-      console.log(itemBox.parentNode);
-      delModel.style.display = "none";
-      itemBox.parentNode.removeChild(itemBox);
-      todosList.splice(index, 1);
-      window.localStorage.setItem("todos", JSON.stringify(todosList));
-      todosList = JSON.parse(localStorage.getItem("todos")) || [];
-      notification.innerHTML = "Deleted successfully";
-      setTimeout(function () {
-        notification.innerHTML = "";
-      }, 2000);
-    });
-    delCanBtn.addEventListener("click", () => {
-      delModel.style.display = "none";
-    });
-    delClose.addEventListener("click", () => {
-      delModel.style.display = "none";
-    });
-  } else {
-    //delete task incomplete
-    delModelContent.innerHTML = "Ypur task is inComplete?Are your Sure?";
-    delModel.style.display = "block";
-    delBtn.addEventListener("click", () => {
-      itemBox.parentNode.removeChild(itemBox);
-      todosList.splice(index, 1);
-      window.localStorage.setItem("todos", JSON.stringify(todosList));
-      todosList = JSON.parse(localStorage.getItem("todos")) || [];
-      delModel.style.display = "none";
-      notification.innerHTML = "Deleted successfully";
-      setTimeout(function () {
-        notification.innerHTML = "";
-      }, 2000);
-    });
-    delCanBtn.addEventListener("click", () => {
-      delModel.style.display = "none";
-    });
-    delClose.addEventListener("click", () => {
-      delModel.style.display = "none";
-    });
-  }
-  //displayTask();
+	var delModel = document.querySelector(".modal");
+	var delModelContent = document.querySelector(".delP");
+	var delBtn = document.querySelector(".del1");
+	var delCanBtn = document.querySelector(".cancel1");
+	var delClose = document.querySelector(".close");
+	delCanBtn.onclick = () => {
+		delModel.style.display = "none";
+	};
+	delClose.onclick = () => {
+		delModel.style.display = "none";
+	};
+	var index = -1;
+	for (var i = 0; i < todosList.length; i++) {
+		if (todosList[i].text === todoText) {
+			index = i;
+			break;
+		}
+	}
+	if (todosList[index].completed === true) {
+		delModelContent.innerHTML = "Are your Sure?";
+		delModel.style.display = "block";
+		delBtn.onclick = () => {
+			delAction(itemBox, index, delModel);
+		};
+	} else {
+		delModelContent.innerHTML = "Your task is inComplete?Are your Sure?";
+		delModel.style.display = "block";
+		delBtn.onclick = () => {
+			delAction(itemBox, index, delModel);
+		};
+	}
 }
-
+function delAction(itemBox, index, delModel) {
+	itemBox.parentNode.removeChild(itemBox);
+	todosList.splice(index, 1);
+	window.localStorage.setItem("todos", JSON.stringify(todosList));
+	todosList = JSON.parse(localStorage.getItem("todos")) || [];
+	delModel.style.display = "none";
+	delMsg((msg = "Deleted"));
+}
+function delMsg(msg) {
+	notification.innerHTML = msg + " Successfully";
+	setTimeout(function () {
+		notification.innerHTML = "";
+	}, 2000);
+}
+//complete sts update
+function updateTaskToLocalStore() {
+	const todosEl = document.querySelectorAll(".item_input");
+	const todos = [];
+	todosEl.forEach((todoEl) => {
+		todos.push({
+			text: todoEl.innerHTML,
+			completed: todoEl.classList.contains("completed"),
+		});
+	});
+	localStorage.setItem("todos", JSON.stringify(todos));
+	todosList = JSON.parse(localStorage.getItem("todos"));
+}
+//active nav bars
 var btnContainer = document.getElementsByClassName("nav-btn");
 var btns = document.getElementsByClassName("btn");
-console.log(btns);
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+	btns[i].addEventListener("click", function () {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
-function updateTaskToLocalStore() {
-  const todosEl = document.querySelectorAll(".item_input");
-  const todos = [];
-  todosEl.forEach((todoEl) => {
-    todos.push({
-      text: todoEl.innerHTML,
-      completed: todoEl.classList.contains("completed"),
-    });
-  });
-  localStorage.setItem("todos", JSON.stringify(todos));
-  todosList = JSON.parse(localStorage.getItem("todos"));
-}
-
+//nav bar function
 selctorNav.forEach((item) =>
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    var currentActiveNav = document.getElementsByClassName("active");
-    //console.log(currentActiveNav);
-    switch (e.target.id) {
-      case "completed":
-        console.log("complete");
-        container.style.display = "none";
-        container3.style.display = "none";
-        notes.style.display = "none";
-        container2.innerHTML = "";
-        for (var i = 0; i < todosList.length; i++) {
-          if (todosList[i].completed === true) {
-            var todoText = todosList[i].text;
-            var itemBox = document.createElement("div");
-            itemBox.classList.add("item");
-
-            var todoEl = document.createElement("li");
-            todoEl.innerHTML = todoText;
-            todoEl.classList.add("item_input1");
-            todoEl.id = "placing";
-            container2.appendChild(itemBox);
-            itemBox.append(todoEl);
-          }
-        }
-        if (container2.innerHTML == "") {
-          container2.innerHTML = "No Completed Task";
-          container2.style.display = "block";
-        } else {
-          container2.style.display = "block";
-        }
-        break;
-      case "incomplete":
-        console.log("inComplete");
-        container.style.display = "none";
-        container2.style.display = "none";
-        container3.innerHTML = "";
-        notes.style.display = "none";
-        for (var i = 0; i < todosList.length; i++) {
-          if (todosList[i].completed === false) {
-            var todoText = todosList[i].text;
-            var itemBox = document.createElement("div");
-            itemBox.classList.add("item");
-
-            var todoEl = document.createElement("li");
-            todoEl.innerHTML = todoText;
-            todoEl.classList.add("item_input1");
-            todoEl.id = "placing";
-            container3.appendChild(itemBox);
-            itemBox.append(todoEl);
-          }
-        }
-        if (container3.innerHTML == "") {
-          container3.innerHTML = "No Incomplete Task";
-          container3.style.display = "block";
-        } else {
-          container3.style.display = "block";
-        }
-        break;
-      case "total":
-        displayTask();
-        break;
-    }
-  })
+	item.addEventListener("click", (e) => {
+		switch (e.target.id) {
+			case "completed":
+				var dataItem = document.querySelectorAll(".item_input");
+				for (i = 0; i < dataItem.length; i++) {
+					if (dataItem[i].classList.contains("completed")) {
+						dataItem[i].parentNode.style.display = "block";
+					} else {
+						dataItem[i].parentNode.style.display = "none";
+					}
+				}
+				break;
+			case "incomplete":
+				var dataItem = document.querySelectorAll(".item_input");
+				for (i = 0; i < dataItem.length; i++) {
+					if (dataItem[i].classList.contains("completed")) {
+						dataItem[i].parentNode.style.display = "none";
+					} else {
+						dataItem[i].parentNode.style.display = "block";
+					}
+				}
+				break;
+			case "total":
+				displayTask();
+				break;
+		}
+	})
 );
